d2b0b6e347dce1a2851103d7d0d6759743f2c585
soobin <lluvias988@gmail.com>
2019-09-29 12:55:56 +0900

    speech1

--- app/src/main/java/com/example/e_app/MainActivity.kt
+++ app/src/main/java/com/example/e_app/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.e_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    val button: Button=findViewById(R.id.list1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            val intent=Intent(this,StudyActivity::class.java)
+            startActivityForResult(intent,1)
+            //startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/e_app/StudyActivity.kt
@@ -0,0 +1,14 @@
+package com.example.e_app
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StudyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_study)
+
+    }
+}
